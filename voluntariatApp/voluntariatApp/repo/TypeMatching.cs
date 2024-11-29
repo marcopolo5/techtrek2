acb0e8f395ae83d540c9ff1e1d9cc4d5c455b204
zdrobix <zdroba88@gmail.com>
2024-11-29 12:31:34 +0200

    Functions to create entity from list.
    Still contemplating wheter this should have been done from the domain classes. like a static function:
          public static User getUserFromList (List<string> args)

--- voluntariatApp/voluntariatApp/repo/TypeMatching.cs
+++ voluntariatApp/voluntariatApp/repo/TypeMatching.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using voluntariatApp.domain;
+using voluntariatApp.domain.enums;
 
 namespace voluntariatApp.repo
 {
-	internal static class TypeMatching
+	internal class TypeMatching<E, ID> where E : Entity<ID>
 	{
-
 		public static string returnTableName (Type type)
 		{
 			if (type == typeof(User))
@@ -39,6 +40,43 @@ namespace voluntariatApp.repo
 			else throw new ArgumentException("Invalid entity type " + type.Name);
 		}
 
+		public static E? createEntityFromList (Type type, List<string> parameters)
+		{
+			if (type == typeof(User))
+			{
+				return new User(
+					parameters[0],
+					parameters[1],
+					(Occupation)Enum.Parse(typeof(Occupation), parameters[3])
+				) as E;
+			}
+			else if (type == typeof(Organiser))
+			{
+				return new Organiser(
+					parameters[0],
+					parameters[1],
+					(OrganiserType)Enum.Parse(typeof(OrganiserType), parameters[2]),
+					parameters[3]
+				) as E;
+			}
+			else if (type == typeof(LoginEntity))
+			{
 
+			}
+			else if (type == typeof(Event))
+			{
+
+			}
+			else if (type == typeof(EventSignup))
+			{
+
+			}
+			else if (type == typeof(Participation))
+			{
+
+			}
+			else throw new ArgumentException("Invalid entity type " + type.Name);
+			return null;
+		}
 	}
 }
