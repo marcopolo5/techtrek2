ddc69a2fb6e280112bdabda45e50d44d5c79cbc7
zdrobix <zdroba88@gmail.com>
2024-11-29 11:43:00 +0200

    Changed ID type. From long to Tuple(string, long).
    Because a participation is between a user and an event. Many to many relation.

--- voluntariatApp/voluntariatApp/domain/Participation.cs
+++ voluntariatApp/voluntariatApp/domain/Participation.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace voluntariatApp.domain
 {
-    internal class Participation : Entity<long>
+    internal class Participation : Entity<Tuple<string, long>>
     {
         private bool Present { get; init; }
         private string Feedback { get; init; }
 
-        public Participation(long id, bool present, string feedback)
+        public Participation(string cnp, long id, bool present, string feedback)
         {
             this.Present = present;
             this.Feedback = feedback;
-            this.setId(id);
+            this.setId(Tuple.Create(cnp, id));
         }
     }
 }
