ca001f53cbfec51b69cedc25ab7aa8d6dfcd0063
zdrobix <zdroba88@gmail.com>
2024-11-29 11:21:34 +0200

    Added IronXL package to read from Excel.
    This is temporary as the goal is for the data to be read from an actual database.

--- voluntariatApp/voluntariatApp/repo/Repository.cs
+++ voluntariatApp/voluntariatApp/repo/Repository.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using voluntariatApp.domain;
 
 namespace voluntariatApp.repo
 {
-	internal class Repository
+	internal class Repository<E, ID> where E : Entity<ID>
 	{
+		public Repository() {
+			if (typeof(E) == typeof(User))
+			{
+
+			}
+			else if (typeof(E) == typeof(Organiser))
+			{
+
+			}
+			else if (typeof(E) == typeof(LoginEntity))
+			{
+
+			}
+			else if (typeof(E) == typeof(Event))
+			{
+
+			}
+			else if (typeof(E) == typeof(EventSignup))
+			{
+
+			}
+			else if (typeof(E) == typeof(Participation))
+			{
+
+			}
+		}
+
+		public E? Find(ID id)
+		{
+		
+			return null;
+
+		}
+		public IEnumerable<E>? FindAll()
+		{
+			return null;
+		}
+		public void Delete(ID id)
+		{
+
+		}
+		public E? Update(E entity)
+		{
+			return null;
+		}
 	}
 }
