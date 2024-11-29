788e4971fab3bb461f6a433374ee2654c5196edc
zdrobix <zdroba88@gmail.com>
2024-11-29 12:31:55 +0200

    Repository fixing. Find function almost done. Not tested.

--- voluntariatApp/voluntariatApp/Form1.cs
+++ voluntariatApp/voluntariatApp/Form1.cs
@@ -15,10 +15,6 @@ namespace voluntariatApp
 
 		private void loadDb()
 		{
-			WorkBook wb = WorkBook.Load(@"Q:\info\ProiectTechTrek\voluntariatApp\voluntariatApp\Baza de date.xlsx");
-			var sheet1 = wb.GetWorkSheet("Logins");
-
-			this.showDataLabel.Text = sheet1.Rows[1].Columns[0].ToString();
 
 		}
 	}
