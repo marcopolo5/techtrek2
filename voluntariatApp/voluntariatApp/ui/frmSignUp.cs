09625e6745c78e101ac2f7e1305ccee34d2b7031
zdrobix <zdroba88@gmail.com>
2024-12-16 14:44:01 +0200

    Merged Controller with BackendFrontend.

--- voluntariatApp/voluntariatApp/ui/frmSignUp.cs
+++ voluntariatApp/voluntariatApp/ui/frmSignUp.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using voluntariatApp.test;
 
 namespace voluntariatApp
 {
@@ -26,9 +27,30 @@ namespace voluntariatApp
 
         private void btnOrganizator_Click(object sender, EventArgs e)
         {
-            frmSignUpOrganizator form1 = new frmSignUpOrganizator();
+            Controller controller = Controller.getInstance();
+
+            controller.addData(new List<string> { txtUsername.Text, txtPassword.Text, txtPhone.Text, txtEmail.Text });
+
+            frmSignUpOrganizator signup = new frmSignUpOrganizator();
+            this.Hide();
+            signup.Show();
+        }
+
+        private void frmSignUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVoluntar_Click(object sender, EventArgs e)
+        {
+
+            Controller controller = Controller.getInstance();
+
+            controller.addData(new List<string> { txtUsername.Text, txtPassword.Text, txtPhone.Text, txtEmail.Text });
+
+            frmSignUpVoluntar signUpVoluntar = new frmSignUpVoluntar();
             this.Close();
-            form1.Show();
+            signUpVoluntar.Show();
         }
     }
 }
--- voluntariatApp/voluntariatApp/ui/frmSignUpOrganizator.cs
+++ voluntariatApp/voluntariatApp/ui/frmSignUpOrganizator.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using voluntariatApp.domain.enums;
+using voluntariatApp.test;
 
 namespace voluntariatApp
 {
@@ -26,6 +29,9 @@ namespace voluntariatApp
 
         private void btnCreeazaContV_Click(object sender, EventArgs e)
         {
+            Controller controller = Controller.getInstance();
+            controller.signUpOrganizator(new List<string> { txtCUI.Text, txtName.Text, cmbType.Text, rtxtDescription.Text });
+            MessageBox.Show("Contul a fost creat cu succes!");
             frmLogin form1 = new frmLogin();
             this.Close();
             form1.Show();
@@ -33,7 +39,7 @@ namespace voluntariatApp
 
         private void frmSignUpOrganizator_Load(object sender, EventArgs e)
         {
-
+            cmbType.DataSource = Enum.GetValues(typeof(OrganiserType));
         }
     }
 }
